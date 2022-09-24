f2e9a06d3e8ebf58008c7dcdabd3be278309df6f
Joris <faventox@gmail.com>
2022-09-24 17:33:44 +0200

    Added ;

--- script.js
+++ script.js
@@ -1,39 +1,44 @@
-let choices = ['Rock', 'Paper', 'Scissors']
+let choices = ['Rock', 'Paper', 'Scissors'];
 
-function getComputerChoice () {
-    return choices[Math.floor(Math.random() * choices.length)]
+function getComputerChoice() {
+    return choices[Math.floor(Math.random() * choices.length)];
 }
 
-function playRound (playerSelection, computerSelection) {
+function playRound(playerSelection, computerSelection) {
+
     // Wins
     if (playerSelection.toLowerCase() === 'rock' && computerSelection.toLowerCase() === 'scissors') {
-        return 'You Win! Rock beats Scissors'
+        return 'You Win! Rock beats Scissors';
     }
     else if (playerSelection.toLowerCase() === 'paper' && computerSelection.toLowerCase() === 'rock') {
-        return 'You Win! Paper beats Rock'
+        return 'You Win! Paper beats Rock';
     }
     else if (playerSelection.toLowerCase() === 'scissors' && computerSelection.toLowerCase() === 'paper') {
-        return 'You Win! Scissors beats Paper'
+        return 'You Win! Scissors beats Paper';
     }
 
-    // Draw
+    // Draws
     if (playerSelection.toLowerCase() === computerSelection.toLowerCase()) {
-        return `Draw! ${playerSelection} and ${computerSelection} are the same choices`
+        return `Draw! ${playerSelection} and ${computerSelection} are the same choices`;
     }
 
     // Losses
     if (playerSelection.toLowerCase() === 'scissors' && computerSelection.toLowerCase() === 'rock') {
-        return 'You Lose! Rock beats Scissors'
+        return 'You Lose! Rock beats Scissors';
     }
     else if (playerSelection.toLowerCase() === 'rock' && computerSelection.toLowerCase() === 'paper') {
-        return 'You Lose! Paper beats Rock'
+        return 'You Lose! Paper beats Rock';
     }
     else if (playerSelection.toLowerCase() === 'paper' && computerSelection.toLowerCase() === 'scissors') {
-        return 'You Lose! Scissors beats Paper'
+        return 'You Lose! Scissors beats Paper';
+    }
+}
+const playerSelection = 'rock';
+const computerSelection = getComputerChoice();
+
+function game() {
+    for (let i = 0; i < 5; i++) {
+        console.log(playRound());
     }
 }
 
-const playerSelection = prompt('Choose between Rock, Paper or Scissors!')
-const computerSelection = getComputerChoice();
-console.log(getComputerChoice());
-console.log(playRound(playerSelection, computerSelection));
