cfa077220514e27e41d9b9ab3d0bf61030b37c48
Joris <faventox@gmail.com>
2022-09-24 14:08:24 +0200

    Added working playRound function that returns Win/Loss/Draw

--- script.js
+++ script.js
@@ -4,4 +4,36 @@ function getComputerChoice () {
     return choices[Math.floor(Math.random() * choices.length)]
 }
 
+function playRound (playerSelection, computerSelection) {
+    // Wins
+    if (playerSelection.toLowerCase() === 'rock' && computerSelection.toLowerCase() === 'scissors') {
+        return 'You Win! Rock beats Scissors'
+    }
+    else if (playerSelection.toLowerCase() === 'paper' && computerSelection.toLowerCase() === 'rock') {
+        return 'You Win! Paper beats Rock'
+    }
+    else if (playerSelection.toLowerCase() === 'scissors' && computerSelection.toLowerCase() === 'paper') {
+        return 'You Win! Scissors beats Paper'
+    }
+
+    // Draw
+    if (playerSelection.toLowerCase() === computerSelection.toLowerCase()) {
+        return `Draw! ${playerSelection} and ${computerSelection} are the same choices`
+    }
+
+    // Losses
+    if (playerSelection.toLowerCase() === 'scissors' && computerSelection.toLowerCase() === 'rock') {
+        return 'You Lose! Rock beats Scissors'
+    }
+    else if (playerSelection.toLowerCase() === 'rock' && computerSelection.toLowerCase() === 'paper') {
+        return 'You Lose! Paper beats Rock'
+    }
+    else if (playerSelection.toLowerCase() === 'paper' && computerSelection.toLowerCase() === 'scissors') {
+        return 'You Lose! Scissors beats Paper'
+    }
+}
+
+const playerSelection = prompt('Choose between Rock, Paper or Scissors!')
+const computerSelection = getComputerChoice();
 console.log(getComputerChoice());
+console.log(playRound(playerSelection, computerSelection));
